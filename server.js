bb4bbad0e208ef98e1cd979d8ac81fc0388a6ad6
Jonathan Benson <jbensonamdg@gmail.com>
2022-05-06 18:59:37 -0500

    Added .env file for configuration

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
+
+/*
+
+.env file should have the following variables defined:
+
+DB_USERNAME
+DB_PASSWORD
+DB_NAME
+DB_PORT
+
+
+*/
+
+const port = process.env.DB_PORT;
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
