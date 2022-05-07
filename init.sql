9bc05eb6585ce40c71c873af0a0ebe28cc63dd2f
Jonathan Benson <jbensonamdg@gmail.com>
2022-05-07 01:02:03 -0500

    Removed POST_RATING table and RATE_POST procedure in anticipation for some different features to come.

--- init.sql
+++ init.sql
@@ -30,22 +30,6 @@ CREATE TABLE POST (
     
 );
 
-CREATE TABLE POST_RATING (
-	-- Ratings by users on posts
-    
-	AgentUsername VARCHAR(16) NOT NULL,
-    PostTitle VARCHAR(32) NOT NULL,
-    
-    -- Users can rate a post as 1 (like) or -1 (dislike)
-    Rating INT NOT NULL CHECK(Rating = -1 OR Rating = 1),
-    
-    -- A user can only rate the post once (but can update their one rating later)
-    PRIMARY KEY (AgentUsername, PostTitle),
-    FOREIGN KEY (AgentUsername) REFERENCES AGENT (Username),
-    FOREIGN KEY (PostTitle) REFERENCES POST (Title)
-    
-);
-
 DELIMITER $$
 
 CREATE PROCEDURE CREATE_AGENT (IN agentUsername VARCHAR(16), IN passwordHash CHAR(64), OUT wasSuccess BOOL)
@@ -145,38 +129,6 @@ BEGIN
     END;
     END IF;
 
-END $$
-
-CREATE PROCEDURE RATE_POST (IN agentUsername VARCHAR(16), IN postTitle VARCHAR(32), IN postRating INT)
-BEGIN
-    /*
-
-    Stored procedure to submit/update a rating for a post in the POST_RATING table.
-
-    */
-
-	-- check if the user has already rated the post
-	IF (NOT EXISTS (SELECT Rating FROM POST_RATING WHERE PostTitle = postTitle AND AgentUsername = agentUsername)) THEN
-	BEGIN
-	
-		-- if the user has NOT already rated the post
-		-- then insert a new rating for the post
-		INSERT INTO POST_RATING (AgentUsername, PostTitle, Rating)
-		VALUES (agentUsername, postTitle, postRating);
-	
-	END;
-	ELSE
-	BEGIN
-	
-		-- if the user already rated the post
-		-- then simply update their rating to the new one
-		UPDATE POST_RATING
-		SET Rating = postRating
-		WHERE AgentUsername = agentUsername AND PostTitle = postTitle;
-		
-	END;
-	END IF;
-        
 END $$
 
 
