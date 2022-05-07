149538bd9f89f4dc4939d55890f35a5810af61ae
Jonathan Benson <jbensonamdg@gmail.com>
2022-05-06 19:06:31 -0500

    More initialization

--- server.js
+++ server.js
@@ -12,19 +12,27 @@ dotenv.config();
 DB_USERNAME
 DB_PASSWORD
 DB_NAME
-DB_PORT
+PORT
 
 
 */
 
-const port = process.env.DB_PORT;
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+const port = process.env.PORT;
+
+console.log("DB_USERNAME: " + dbUsername);
+console.log("DB_PASSWORD: " + dbPassword);
+console.log("DB_NAME: " + dbName);
+console.log("DB_PORT: " + port + "\n\n");
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+	res.send("Hello World!");
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+	console.log(`App listening on port ${port}...`)
 })
 
 
