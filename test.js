e88776e04a495c2bb905d6e731a1d18b3912c0fb
Jonathan Benson <jbensonamdg@gmail.com>
2022-05-07 12:05:42 -0500

    Created test for CREATE_AGENT stored procedure

--- test.js
+++ test.js
@@ -11,12 +11,68 @@ const {
 
 describe('stored procedures', () => {
 
+    // before each and after all tests clean the database
     beforeEach(() => query('CALL CLEAN ()'));
     afterAll(() => query('CALL CLEAN ()'));
 
-    test('first test', () => {
+    test('CREATE_AGENT', () => {
 
-        expect(true).toEqual(true);
+        const username = 'jonathan';
+        const passwordHash = hash('benson', secret);
+
+        return query(`
+
+            SET @wasSuccess = FALSE;
+
+            CALL CREATE_AGENT('${username}', '${passwordHash}', @wasSuccess);
+
+            SELECT @wasSuccess AS wasSuccess;
+
+            SELECT Username AS username, PasswordHash AS passwordHash
+            FROM AGENT
+            WHERE Username = '${username}' AND PasswordHash = '${passwordHash}'
+            LIMIT 1;
+
+        `).then(result => {
+            /*
+
+            Create a new user for the first time.
+
+            Expect user to be inserted into the database, with no errors.
+
+            */
+
+            const wasSuccess = result[2][0].wasSuccess;
+            const agentUsername = result[3][0].username;
+            const agentPasswordHash = result[3][0].passwordHash;
+
+            expect(wasSuccess).toEqual(1);
+            expect(username).toEqual(agentUsername);
+            expect(passwordHash).toEqual(agentPasswordHash);
+
+
+        }).then(() => query(`
+
+            SET @wasSuccess = FALSE;
+
+            CALL CREATE_AGENT('${username}', '${passwordHash}', @wasSuccess);
+
+            SELECT @wasSuccess AS wasSuccess;
+
+        `)).then(result => {
+            /*
+
+            Try to create the same user a second time.
+
+            Expect no changes to the database, and an error.
+
+            */
+           
+            const wasSuccess = result[2][0].wasSuccess;
+            
+            expect(wasSuccess).toEqual(0);
+
+        });
 
     });
 
